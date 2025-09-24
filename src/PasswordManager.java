158be089b353bb6336ede4f23dcb6bce6bb02e1b
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-09-23 22:23:41 -0300

    Se crea el método menu que posee el flujo de código, el método mostrarMenu para ver las opciones del menú por consola, y los demas método para que el programa no presente errores.

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,9 +1,61 @@
+import java.util.Scanner;
+
 public class PasswordManager {
     public static void main(String[] args) {
-        String saludo;
-        saludo = "Hola papus";
+        menu();
+    }
 
-        System.out.println(saludo);
-        //Salida -> Hola papus
+    public static void mostrarMenu(){
+        System.out.println("""
+                ---Bienvenido al menu de PassMan---
+                
+                1. Agregar contraseña
+                2. Eliminar contraseña
+                3. Actualizar contraseña
+                4. Ver contraseñas
+                5. Salir
+                
+                Seleccione una opcion:""");
+    }
+
+    public static void menu(){
+        Scanner entrada = new Scanner(System.in);
+        boolean salir = false;
+
+        do {
+            mostrarMenu();
+
+            //Pide un dato por consola y lo guarda en una variable.
+            String opcion = entrada.nextLine();
+
+            //Permite eligir una opcion por consola en base a un dato.
+            switch (opcion) {
+                case "1" -> agregarContrasena();
+                case "2" -> eliminarContrasena();
+                case "3" -> actualizarContrasena();
+                case "4" -> verContrasenas();
+                case "5" -> salir = true;
+                default -> System.out.println("Opcion invalida. Intente nuevamente");
+            }
+        }
+
+        while (!salir);
+        System.out.println("Gracias por usar la PassMan...");
+    }
+
+    public static void agregarContrasena(){
+        System.out.println("Aquí se agregan las cotraseñas");
+    }
+
+    public static void eliminarContrasena(){
+        System.out.println("Aquí se liminan las contraseñas");
+    }
+
+    public static void actualizarContrasena(){
+        System.out.println("Aquí se actualizan las cotraseñas");
+    }
+
+    public static void verContrasenas(){
+        System.out.println("Aquí se muestran las cotraseñas");
     }
 }
