db5d0eab04236774655938e6c6c2b457214948f2
1uisardinh0 <l.villalobos03@ufromail.cl>
2025-09-23 21:14:18 -0300

    Añadiendo algunas cosillas

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,5 +1,9 @@
 public class PasswordManager {
     public static void main(String[] args) {
+        String saludo;
+        saludo = "Hola papus";
 
+        System.out.println(saludo);
+        //Salida -> Hola papus
     }
 }
