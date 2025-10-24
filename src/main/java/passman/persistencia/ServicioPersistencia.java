eccc9267d1bd3312875360384929f7f9f51fe043
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-10-23 22:47:40 -0300

    Agrege ServicioPersistencia

--- src/main/java/passman/persistencia/ServicioPersistencia.java
+++ src/main/java/passman/persistencia/ServicioPersistencia.java
@@ -1,4 +1,12 @@
 package passman.persistencia;
+import passman.Config;
+import passman.modelo.EntradaCredencial;
+import passman.modelo.Usuario;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class ServicioPersistencia {
     
