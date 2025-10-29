4fc4ad228903f84f87edbbf9080e3daa8bd52b02
Seba <sebastian.c2005r@gmail.com>
2025-10-29 01:25:06 -0300

    Se incorporá nuevo ServicioUsuarios que se encarga de gestionar los datos de usuarios, creando, recuperando y manipulando estos datos.

---
+++ src/main/java/passman/servicio/ServicioUsuarios.java
@@ -0,0 +1,40 @@
+package passman.servicio;
+
+import passman.modelo.Usuario;
+import passman.cifrado.ServicioCifrado;
+import passman.persistencia.ServicioPersistencia;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class ServicioUsuarios {
+    private final ServicioPersistencia persistencia;
+    private final ServicioCifrado cifrador;
+
+    public ServicioUsuarios(ServicioPersistencia persistencia, ServicioCifrado cifrador) {
+        this.persistencia = persistencia;
+        this.cifrador = cifrador;
+    }
+
+    public boolean crearUsuario(String nombreUsuario, String rut, String fechaNac, String passwordHash, String salt, int iteraciones) {
+        String rutCifrado = cifrador.cifrar(rut);
+        String fechaNacCifrado = cifrador.cifrar(fechaNac);
+
+        Usuario usuario = new Usuario(nombreUsuario, passwordHash, salt, iteraciones);
+
+        usuario.setRutCifrado(rutCifrado);
+        usuario.setFechaNacCifrado(fechaNacCifrado);
+
+        return persistencia.guardarUsuario(usuario);
+    }
+
+    public Usuario obtenerUsuario(String nombreUsuario) {
+        Optional<Usuario> usuarioOpt = persistencia.buscarUsuarioPorNombre(nombreUsuario);
+
+        return usuarioOpt.orElse(null);
+    }
+
+    public boolean existeUsuario(String nombreUsuario) {
+        return  persistencia.buscarUsuarioPorNombre(nombreUsuario).isPresent();
+    }
+}
