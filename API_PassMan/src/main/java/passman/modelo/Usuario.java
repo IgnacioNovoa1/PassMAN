20712c2e9f59c9a3fdf9ecb70bd6c7d7b6888f78
IgnacioNovoa1 <ignacionovoa8@gmail.com>
2025-11-25 21:06:47 -0300

    Adapte el modelo usuario a la nueva utilidad

--- API_PassMan/src/main/java/passman/modelo/Usuario.java
+++ API_PassMan/src/main/java/passman/modelo/Usuario.java
@@ -1,5 +1,6 @@
 package passman.modelo;
 import java.util.UUID;
+
 public class Usuario {
     //Datos Inicio Sesión
     private final UUID idUsuario;
@@ -13,10 +14,12 @@ public class Usuario {
     private String rutCifrado;
     private String fechaNacCifrada;
     private String ivPersonales;
+    private String codigoRecuperacion;
     
     ///CONSTRUCTOR usuario NUEVO
     public Usuario(String nomUser, String passHash, String salt, int iterac){
         this.idUsuario = UUID.randomUUID();
+        this.nombreUsuario = nomUser; 
         this.passwordHash = passHash;
         this.salt = salt;
         this.iteraciones = iterac;
@@ -30,59 +33,63 @@ public class Usuario {
         this.iteraciones = iterac;
     }
     ///GETTERS y SETTERS
-    public UUID getIdUsuario(){
-        return idUsuario;
+    public UUID getIdUsuario(){ 
+        return idUsuario; 
     }
-    public String getNombreUsuario(){
-        return nombreUsuario;
+    public String getNombreUsuario(){ 
+        return nombreUsuario; 
     }
-    public String getPasswordHash(){
-        return passwordHash;
+    public String getPasswordHash(){ 
+        return passwordHash; 
     }
-    public String getSalt (){
-        return salt;
+    public String getSalt (){ 
+        return salt; 
     }
-    public int getIteraciones() {
-        return iteraciones;
+    public int getIteraciones() { 
+        return iteraciones; 
     }
-    public String getNombreCifrado() {
-        return nombreCifrado;
+    public String getNombreCifrado() { 
+        return nombreCifrado; 
     }
-    public String getApellidoCifrado(){
-        return apellidoCifrado;
+    public String getApellidoCifrado(){ 
+        return apellidoCifrado; 
     }
-    public String getRutCifrado(){
-        return rutCifrado;
+    public String getRutCifrado(){ 
+        return rutCifrado; 
     }
-    public String getFechaNacCifrada(){
-        return fechaNacCifrada;
+    public String getFechaNacCifrada(){ 
+        return fechaNacCifrada; 
     }
-    public String getIvPersonales(){
-        return ivPersonales;
+    public String getIvPersonales(){ 
+        return ivPersonales; 
     }
-    public void setPasswordHash(String passHash){
-        this.passwordHash = passHash;
-    }
-    public void setSalt(String salt){
-        this.salt = salt;
-    }
-    public void setIteraciones(int iterac){
-        this.iteraciones = iterac;
-    }
-    public void setNombreCifrado(String nom){
-        this.nombreCifrado = nom;
-    }
-    public void setApellidoCifrado(String apell){
-        this.apellidoCifrado = apell;
-    }
-    public void setRutCifrado(String rutCifrado){
-        this.rutCifrado = rutCifrado;
-    }
-    public void setFechaNacCifrado(String fechaNacCifrada){
-        this.fechaNacCifrada = fechaNacCifrada;
-    }
-    public void setIvPersonales(String ivPersonales){
-        this.ivPersonales = ivPersonales;
-    }
-}
+    public String getCodigoRecuperacion() { return codigoRecuperacion; } 
 
+    public void setPasswordHash(String passHash){
+        this.passwordHash = passHash; 
+    }
+    public void setSalt(String salt){ 
+        this.salt = salt; 
+    }
+    public void setIteraciones(int iterac){ 
+        this.iteraciones = iterac; 
+    }
+    public void setNombreCifrado(String nom){ 
+        this.nombreCifrado = nom; 
+    }
+    public void setApellidoCifrado(String apell){ 
+        this.apellidoCifrado = apell; 
+    }
+    public void setRutCifrado(String rutCifrado){ 
+        this.rutCifrado = rutCifrado; 
+    }
+    public void setFechaNacCifrada(String fechaNacCifrada){ 
+        this.fechaNacCifrada = fechaNacCifrada; 
+    }
+    public void setIvPersonales(String ivPersonales){ 
+        this.ivPersonales = ivPersonales; 
+    }
+    public void setCodigoRecuperacion(String codigo) { 
+        this.codigoRecuperacion = codigo; 
+    } 
+}
